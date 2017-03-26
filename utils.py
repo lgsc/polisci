cb0a203990dde139be92b6f701e91b97c48113b2
Gustavo <lgsc@users.noreply.github.com>
2017-03-26 11:41:49 -0500

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,18 @@
+
+
+
+def make_soup(url):
+    thepage = urllib.request.urlopen(url)
+    soupdata = BeautifulSoup(thepage, "html.parser")
+    return soupdata
+
+def get_souptable(url):
+    soup = make_soup(url)
+    data_dict = dict()
+    for row in soup.findAll('tr'):
+        i = -1
+        for column in soup.findAll('td'):
+            i = i + 1
+            b = str(i)
+            data_dict[b] = column.text
+    return data_dict
